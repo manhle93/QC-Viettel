995e014abc0c306de4ab972eec8528b2e059f253
manhle93 <zerokisss@gmail.com>
2021-03-16 22:59:03 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import HomePage from './page/HomePage'
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Viettel Hung Yen</h1>
+        </div>
+    )
+}
+
+export default HomePage;
